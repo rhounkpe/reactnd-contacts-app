7a07d12aace976d5a4ca9e06ab09e49103095af7
Rufin Hounkpe <rufin.hounkpe@lampiris.be>
2020-11-30 22:25:19 +0100

    Pass Data Into Components With Props

---
+++ src/ContactList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ContactList extends Component {
+    render() {
+        console.log('Props', this.props);
+        
+        return (
+            <ol className='contact-list'>
+                
+            </ol>
+        );
+    }
+}
+
+export default ContactList;
