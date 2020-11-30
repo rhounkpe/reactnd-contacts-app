89e48eeadf9d3e025517aa73ef39a86764b5b0f4
Rufin Hounkpe <rufin.hounkpe@lampiris.be>
2020-11-30 22:35:50 +0100

    Display data from props

--- src/ContactList.js
+++ src/ContactList.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class ContactList extends Component {
-    render() {
-        console.log('Props', this.props);
-        
-        return (
-            <ol className='contact-list'>
-                
-            </ol>
-        );
-    }
+  render() {
+    // console.log('Props', this.props);
+
+    return (
+      <ol className="contact-list">
+        {this.props.contacts.map((contact) => (
+          <li key={contact.id}>{contact.name}</li>
+        ))}
+      </ol>
+    );
+  }
 }
 
 export default ContactList;
